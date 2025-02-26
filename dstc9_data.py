1de651603137908a90b5ec75b9d4eca5f2563ec5
Carmela Pia Senatore <146263625+carmens0@users.noreply.github.com>
2025-02-26 22:00:07 +0100

    Add files via upload

---
+++ dstc9_data.py
@@ -0,0 +1,57 @@
+from scripts import gpt3score
+import json
+import tqdm 
+import scipy
+import os
+
+
+
+annotations = []
+evaluations = []
+results = []
+
+file_path= r"C:\Users\carme\Desktop\dialogue-reponse\datasets\dstc9_data 2.json"
+model_name="davinci-002"
+api_key="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"
+results_file = r"C:\Users\carme\Desktop\dialogue-reponse\datasets\results.json"
+
+
+
+with open(file_path, "r") as file:
+    dstc9_data = json.load(file)
+
+def evaluate( dialog, gpt3model, api_key, method): 
+    result = gpt3score(dialog, gpt3model, api_key=api_key, method="turn-level")
+    gpt_score=result["raw_score"] 
+    return gpt_score
+
+ 
+
+
+if os.path.exists(results_file):
+    with open(results_file, "r") as file:
+        results = json.load(file)
+else:
+    results = []
+
+for i in tqdm.tqdm(range(len(dstc9_data["contexts"]))):
+    if any(result["dialog_id"] == i for result in results):
+        continue  
+
+    dialog = " ".join(dstc9_data["contexts"][i])
+    dialog = f"{dialog} Response: {dstc9_data['responses'][i]}"
+    print(f"Processing dialog {i}: ...")
+
+    model_evaluation = evaluate(dialog, model_name, api_key, method="turn-level")
+    annotation = dstc9_data["scores"][i]
+
+    result_entry = {
+        "dialog_id": i,
+        "human_score": annotation,
+        "predicted_score": model_evaluation,
+    }
+    results.append(result_entry)
+
+    with open(results_file, "w") as file:
+        json.dump(results, file, indent=4)
+
